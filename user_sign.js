6bbbcfed53dfeaa53a3cb9e44e3fcf8e73fc9259
AIYA861202 <phiphiphi27@gmail.com>
2021-07-24 19:50:04 +0800

    media_edit
    media_edit

--- user_sign.js
+++ user_sign.js
@@ -26,11 +26,11 @@ var app = new Vue({
             let isText = /^[a-zA-Z0-9]+$/;
             if (!isText.test(this.username)) {
                 this.usernameError = true;
-                this.userErrMsg = '請勿包含特殊字元如-*/?,.:;"';
+                this.userErrMsg = '☹ 請勿包含特殊字元如-*/?,.:;"';
             }
             else if (this.username.length > 10) {
                 this.usernameError = true;
-                this.userErrMsg = '請勿超過10個字';
+                this.userErrMsg = '☹ 請勿超過10個字';
             }
             else {
                 this.usernameError = false;
@@ -43,20 +43,21 @@ var app = new Vue({
         let inclde = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,10}$/;
         if (!isText.test(this.password)) {
             this.passwordError = true;
-            this.passErrMsg = '請勿包含特殊字元如-*/?,.:;"';
+            this.passErrMsg = '☹ 請勿包含特殊字元如-*/?,.:;"';
+        }
+        else if (!inclde.test(this.password)) {
+            this.passwordError = true;
+            this.passErrMsg = '☹ 至少包括一個大小寫字母或數字';
         }
         else if (this.password.length < 5) {
             this.passwordError = true;
-            this.passErrMsg = '請勿少於5個字';
+            this.passErrMsg = '☹ 請勿少於5個字';
         }
         else if (this.password.length > 10) {
             this.passwordError = true;
-            this.passErrMsg = '請勿超過10個字';
+            this.passErrMsg = '☹ 請勿超過10個字';
         }
-        else if (!include.test(this.password)) {
-            this.passwordError = true;
-            this.passErrMsg = '至少包括一個大小寫字母或數字';
-        }
+        
         else {
             this.passwordError = false;
         }
@@ -65,7 +66,7 @@ var app = new Vue({
         let isMail = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
         if (!isMail.test(this.email)) {
             this.emailError = true;
-            this.emailErrMsg = 'email格式錯誤';
+            this.emailErrMsg = '☹ 信箱格式錯誤';
         }
         else {
             this.emailError = false;
@@ -73,10 +74,10 @@ var app = new Vue({
         },
     
     tel: function () {
-        let isTel = /^09\\d{8}$/;
+        let isTel = /^09[0-9]{8}$/;
         if (!isTel.test(this.tel)) {
             this.telError = true;
-            this.telErrMsg = '手機號碼格式錯誤'; 
+            this.telErrMsg = '☹ 手機號碼格式錯誤'; 
         }
         else{
             this.telError = false;
